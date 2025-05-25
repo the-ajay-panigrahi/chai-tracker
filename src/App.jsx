fb8ba3846c2680843b2794ce83dde7e096c176f3
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-05-25 12:48:14 +0530

    firebase authentication done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useContext } from "react";
 import ChaiForm from "./components/ChaiForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import History from "./components/History";
 import Statistics from "./components/Statistics";
+import { AuthContext } from "./utils/AuthContext";
 
 const App = () => {
-  const isAuthenticated = false;
+  const { isAuthenticated } = useContext(AuthContext);
 
   return (
     <>
